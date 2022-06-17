1a2748e67954ab214fe8e15b2e3639a549f6b4c2
Fabien Roger <efab.roger@gmail.com>
2022-06-17 12:21:25 -0700

    Add Whichone App

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,25 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import WhichOneApp from "./WhichOneApp";
+import reportWebVitals from "./reportWebVitals";
 
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-fetch("/tokens").then(response => response.json()).then((tokens) => { 
-  fetch("/get_doc").then(response => response.json()).then((doc) => { 
-    root.render(
-      <React.StrictMode>
-        <App tokens={tokens} initialDoc={doc}/>
-      </React.StrictMode>
-    );
-
-   });
-
-
-});
-
-
+const pathname = window.location.pathname;
+console.log(pathname);
+if (pathname === "/whichone") {
+  fetch("/get_comparison")
+    .then((response) => response.json())
+    .then((doc) => {
+      root.render(
+        <React.StrictMode>
+          <WhichOneApp initialComparison={doc} />
+        </React.StrictMode>
+      );
+    });
+} else {
+  fetch("/tokens")
+    .then((response) => response.json())
+    .then((tokens) => {
+      fetch("/get_doc")
+        .then((response) => response.json())
+        .then((doc) => {
+          root.render(
+            <React.StrictMode>
+              <App tokens={tokens} initialDoc={doc} />
+            </React.StrictMode>
+          );
+        });
+    });
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
